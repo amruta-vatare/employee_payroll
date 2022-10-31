a29a3da5d04f79b9c1771ad59f92fff5f4a3cc42
amruta.vatare <vatareamruta615@gmail.com>
2022-10-31 23:27:46 +0530

    added logs and environment variable for database username and password

--- src/main/java/com/bridgelabz/employee_payroll/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employee_payroll/EmployeePayrollApplication.java
@@ -2,12 +2,17 @@ package com.bridgelabz.employee_payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
+		ApplicationContext context =  SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll App Is Started In {} Enviornment",context.getEnvironment().getProperty("enviornment"));
+		log.info("Employee Payroll DB user is {}",context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
